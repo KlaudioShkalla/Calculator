10a3f911e7eb9c2a8be519b5fd424eda52e467f3
Klaudio Shkalla <klashk18@htl-shkoder.com>
2025-01-21 10:09:27 +0100

    third commmt yaml file

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,21 +1,27 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
 
+    Calculator calculator = new Calculator();
     @org.junit.jupiter.api.Test
-    void add() {
+    void testAddition() {
+        assertEquals(5, calculator.add(2, 3), " 5");
     }
 
     @org.junit.jupiter.api.Test
-    void substract() {
+    void testSubtraction() {
+        assertEquals(1, calculator.substract(3, 2), " 1");
     }
 
     @org.junit.jupiter.api.Test
-    void multiply() {
+    void testMultiplication() {
+        assertEquals(6, calculator.multiply(2, 3), " 6");
     }
 
     @org.junit.jupiter.api.Test
-    void divide() {
+    void testDivision() {
+        assertEquals(2, calculator.divide(6, 3), "  2");
     }
 }
