f9f5f65b885f2ca3a75ec17c9bd13e945e6dd13c
Klaudio Shkalla <klashk18@htl-shkoder.com>
2025-01-21 10:01:41 +0100

    Second Commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void substract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+    }
+}
